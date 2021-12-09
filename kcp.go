875f9121a9e72e40dd0f62af149fda6785a2f561
colprog <colprog@gmail.com>
2021-12-10 02:20:34 +0800

    limit fastack, backport from ikcp@a5d82c

--- kcp.go
+++ kcp.go
@@ -7,28 +7,29 @@ import (
 )
 
 const (
-	IKCP_RTO_NDL     = 30  // no delay min rto
-	IKCP_RTO_MIN     = 100 // normal min rto
-	IKCP_RTO_DEF     = 200
-	IKCP_RTO_MAX     = 60000
-	IKCP_CMD_PUSH    = 81 // cmd: push data
-	IKCP_CMD_ACK     = 82 // cmd: ack
-	IKCP_CMD_WASK    = 83 // cmd: window probe (ask)
-	IKCP_CMD_WINS    = 84 // cmd: window size (tell)
-	IKCP_ASK_SEND    = 1  // need to send IKCP_CMD_WASK
-	IKCP_ASK_TELL    = 2  // need to send IKCP_CMD_WINS
-	IKCP_WND_SND     = 32
-	IKCP_WND_RCV     = 32
-	IKCP_MTU_DEF     = 1400
-	IKCP_ACK_FAST    = 3
-	IKCP_INTERVAL    = 100
-	IKCP_OVERHEAD    = 24
-	IKCP_DEADLINK    = 20
-	IKCP_THRESH_INIT = 2
-	IKCP_THRESH_MIN  = 2
-	IKCP_PROBE_INIT  = 7000   // 7 secs to probe window size
-	IKCP_PROBE_LIMIT = 120000 // up to 120 secs to probe window
-	IKCP_SN_OFFSET   = 12
+	IKCP_RTO_NDL       = 30  // no delay min rto
+	IKCP_RTO_MIN       = 100 // normal min rto
+	IKCP_RTO_DEF       = 200
+	IKCP_RTO_MAX       = 60000
+	IKCP_CMD_PUSH      = 81 // cmd: push data
+	IKCP_CMD_ACK       = 82 // cmd: ack
+	IKCP_CMD_WASK      = 83 // cmd: window probe (ask)
+	IKCP_CMD_WINS      = 84 // cmd: window size (tell)
+	IKCP_ASK_SEND      = 1  // need to send IKCP_CMD_WASK
+	IKCP_ASK_TELL      = 2  // need to send IKCP_CMD_WINS
+	IKCP_WND_SND       = 32
+	IKCP_WND_RCV       = 32
+	IKCP_MTU_DEF       = 1400
+	IKCP_ACK_FAST      = 3
+	IKCP_INTERVAL      = 100
+	IKCP_OVERHEAD      = 24
+	IKCP_DEADLINK      = 20
+	IKCP_THRESH_INIT   = 2
+	IKCP_THRESH_MIN    = 2
+	IKCP_PROBE_INIT    = 7000   // 7 secs to probe window size
+	IKCP_PROBE_LIMIT   = 120000 // up to 120 secs to probe window
+	IKCP_SN_OFFSET     = 12
+	IKCP_FASTACK_LIMIT = 8
 )
 
 // monotonic reference time point
@@ -144,6 +145,7 @@ type KCP struct {
 	dead_link, incr                        uint32
 
 	fastresend     int32
+	fastacklimit   int32
 	nocwnd, stream int32
 
 	snd_queue []segment
@@ -184,6 +186,7 @@ func NewKCP(conv uint32, output output_callback) *KCP {
 	kcp.ssthresh = IKCP_THRESH_INIT
 	kcp.dead_link = IKCP_DEADLINK
 	kcp.output = output
+	kcp.fastacklimit = IKCP_FASTACK_LIMIT
 	return kcp
 }
 
@@ -808,7 +811,7 @@ func (kcp *KCP) flush(ackOnly bool) uint32 {
 			needsend = true
 			segment.rto = kcp.rx_rto
 			segment.resendts = current + initialTXRTO
-		} else if segment.fastack >= resent { // fast retransmit
+		} else if segment.fastack >= resent && (seg.xmit <= uint32(kcp.fastacklimit) || kcp.fastacklimit <= 0) { // fast retransmit
 			needsend = true
 			segment.fastack = 0
 			segment.rto = kcp.rx_rto
